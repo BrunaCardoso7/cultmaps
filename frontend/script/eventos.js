8688da058232bd4dafc6ae64af46966e39c10075
BrunaCardoso7 <obitadrawing@gmail.com>
2023-10-06 15:51:05 -0300

    feat: estilização dos cards

--- frontend/script/eventos.js
+++ frontend/script/eventos.js
@@ -9,7 +9,42 @@ axios.get(apiUrl)
         const data = response.data
         console.log(data)
         data.forEach(objeto => {
+            console.log(data)
+            const card = document.createElement('div')
+            card.classList.add('card')
+
+            const img = document.createElement('div')
+            // img.setAttribute("src", objeto)
+            img.classList.add('img')
             
+            card.appendChild(img)
+
+            const descricao = document.createElement('div')
+            descricao.classList.add('desc')
+
+            const wrapperTags = document.createElement('div')
+            wrapperTags.classList.add('wrapper-tags')
+
+            const p_loc = document.createElement('p')
+            p_loc.textContent = objeto.localizacao
+            wrapperTags.appendChild(p_loc)
+            const p_data = document.createElement('p')
+            p_data.textContent = "23-04-2023"
+            wrapperTags.appendChild(p_data)
+            descricao.appendChild(wrapperTags)
+
+            const title = document.createElement('p')
+            title.setAttribute("id", "title")
+            title.textContent = objeto.nome
+            descricao.appendChild(title)
+
+            const desc = document.createElement('p')
+            desc.setAttribute("id", "desc")
+            desc.textContent = objeto.descricao
+            descricao.appendChild(desc)
+
+            card.appendChild(descricao)
+            apiDataElement.appendChild(card)
         });
     })
     .catch(e=>{
