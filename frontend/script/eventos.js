8e569deccd69743ba56c661c375a488d1f05ef38
BrunaCardoso7 <obitadrawing@gmail.com>
2023-10-22 15:16:43 -0300

    slider de imagens e filtragem dos eventos

--- frontend/script/eventos.js
+++ frontend/script/eventos.js
@@ -26,9 +26,9 @@ const swiper = new Swiper(".mySwiper", {
 });
 
 const apiUrl = 'http://localhost:3000/api/services'
-
-const apiDataElement = document.getElementById('api-data')
-
+const section = document.getElementById('section-cards')
+const apiDataElement = document.getElementById('api-data1')
+const apiDataElement2 = document.getElementById('api-data2')
 axios.get(apiUrl)
 .then( response =>{
         const data = response.data
@@ -36,12 +36,14 @@ axios.get(apiUrl)
         data.forEach(objeto => {
             console.log(objeto.categoria)
             console.log(data)
-            if(objeto.categoria === 'Tecnologia Games e Geeks'){
-                //cards 
+                const conteinerCards = document.createElement('div')
+                conteinerCards.classList.add('swiper-wrapper')
+                conteinerCards.classList.add('conteiner-cards')
+
                 const card = document.createElement('div')
                 card.classList.add('swiper-slide')
                 card.classList.add('card')
-
+            
                 const img = document.createElement('img')
                 img.setAttribute("src", objeto.imagem)
                 img.classList.add('img')
@@ -68,24 +70,31 @@ axios.get(apiUrl)
                 wrapperTags.appendChild(p_data)
                 descricao.appendChild(wrapperTags)
                 
-
+            
                 const title = document.createElement('p')
                 title.classList.add('title')
                 title.textContent = objeto.nome
                 descricao.appendChild(title)
-
+            
                 const desc = document.createElement('p')
                 desc.classList.add('desc')
                 desc.textContent = objeto.descricao
                 descricao.appendChild(desc)
-
+            
                 const button = document.createElement('button')
                 button.classList.add('moreInfo')
                 button.textContent = "Mais informações"
                 card.appendChild(descricao)
                 card.appendChild(button)
+                
+            if(objeto.categoria === 'Tecnologia Games e Geeks'){
                 apiDataElement.appendChild(card)
-            } else {
+            }
+            if(objeto.categoria === 'Congresso e Palestras'){
+                apiDataElement2.appendChild(card)
+            }
+            
+            else {
                 console.log('categoria não existente')
             }
            
@@ -93,4 +102,29 @@ axios.get(apiUrl)
     })
     .catch(e=>{
         console.error('Erro: ', e )
+})
+
+
+const moreInfo = document.querySelectorAll(".moreInfo")
+console.log(moreInfo)
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("modal-close");
+moreInfo.forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        if( modal.style.display == "none"){
+            modal.style.display = "block"
+        }else{
+            modal.style.display = "none"
+        }
+        modalClose.addEventListener("click", closeModal)
+        // Fechar o modal quando clicar fora da caixa modal
+        function closeModal() {
+            modal.style.display = "none";
+        }
+        window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+        });
+    })
 })
