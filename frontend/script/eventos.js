278edab9aa1c6e7e653a2a4a5f3078cf93bbd1d0
BrunaCardoso7 <obitadrawing@gmail.com>
2023-11-11 22:09:15 -0300

    modal informações

--- frontend/script/eventos.js
+++ frontend/script/eventos.js
@@ -1,3 +1,5 @@
+// const { default: axios } = require("axios");
+
 const menuButton = document.getElementById('btn-menu');
 const navBar = document.getElementById('nav-bar');
 
@@ -60,217 +62,157 @@ const swiper = new Swiper(".mySwiper", {
     },
 });
 
-const apiUrl = 'https://cultmaps.onrender.com/pictures/'
-const section = document.getElementById('section-cards')
-const apiDataElement = document.getElementById('api-data1')
-const apiDataElement2 = document.getElementById('api-data2')
-const apiDataElement3 = document.getElementById('api-data3')
-axios.get(apiUrl)
-.then( response =>{
-        const data = response.data
-        data.forEach(objeto => {
-            // console.log(objeto.categoria)
-            // console.log(data)
-                const conteinerCards = document.createElement('div')
-                conteinerCards.classList.add('swiper-wrapper')
-                conteinerCards.classList.add('conteiner-cards')
+document.addEventListener('DOMContentLoaded', function(){
+    const apiUrl = 'https://cultmaps.onrender.com/pictures/';
+    const section = document.getElementById('section-cards');
+    const apiDataElement = document.getElementById('api-data1');
+    const apiDataElement2 = document.getElementById('api-data2');
+    const apiDataElement3 = document.getElementById('api-data3');
+    const objectDataApi = [];
 
-                const card = document.createElement('div')
-                card.classList.add('swiper-slide')
-                card.classList.add('card')
-            
-                
-                const img = document.createElement('img')
-                img.setAttribute("src", objeto.src)
-                img.classList.add('img')
-                // console.log(objeto.src)
-                card.appendChild(img)
-                
-                const iconFav = document.createElement('i')
-                iconFav.setAttribute('id', 'btnFav')
-                iconFav.classList.add('fa-star')
-                iconFav.classList.add('fa-regular')
-                card.appendChild(iconFav)
+    axios.get(apiUrl)
+        .then(response => {
+            const data = response.data;
+            console.log(data._id)
+            data.forEach(objeto => {
+                const conteinerCards = document.createElement('div');
+                conteinerCards.classList.add('swiper-wrapper');
+                conteinerCards.classList.add('conteiner-cards');
 
-                const descricao = document.createElement('div')
-                descricao.classList.add('desc')
-                
-                const categoria = document.createElement('p')
-                categoria.textContent = objeto.categoria
-                categoria.classList.add('cat')
-                descricao.appendChild(categoria)
-                
-                const wrapperTags = document.createElement('div')
-                wrapperTags.classList.add('wrapper-tags')
-                
-                
-                const p_loc = document.createElement('p')
-                
-                
-                p_loc.textContent = objeto.localizacao
-                wrapperTags.appendChild(p_loc)
-                
-                const p_data = document.createElement('p')
+                const card = document.createElement('div');
+                card.setAttribute('data-nome', objeto.nome);
+                card.classList.add('swiper-slide');
+                card.classList.add('card');
 
-                const data = objeto.data
-                const dataFormat = new Date(data)
+                const img = document.createElement('img');
+                img.setAttribute("src", objeto.src);
+                img.classList.add('img');
+                card.appendChild(img);
 
-                const ano = dataFormat.getFullYear()
-                const mes = ('0'+(dataFormat.getMonth()+1)).slice(-2)
-                const dia = ('0'+ dataFormat.getDate()).slice(-2)
+                const iconFav = document.createElement('i');
+                iconFav.setAttribute('id', 'btnFav');
+                iconFav.classList.add('fa-star');
+                iconFav.classList.add('fa-regular');
+                card.appendChild(iconFav);
 
-                const dataString = `${dia}-${mes}-${ano} `
-                p_data.textContent = dataString
-                wrapperTags.appendChild(p_data)
-                descricao.appendChild(wrapperTags)
-                
+                const descricao = document.createElement('div');
+                descricao.classList.add('desc');
+
+                const categoria = document.createElement('p');
+                categoria.textContent = objeto.categoria;
+                categoria.classList.add('cat');
+                descricao.appendChild(categoria);
+
+                const wrapperTags = document.createElement('div');
+                wrapperTags.classList.add('wrapper-tags');
+
+                const p_loc = document.createElement('p');
+                p_loc.textContent = objeto.localizacao;
+                wrapperTags.appendChild(p_loc);
+
+                const p_data = document.createElement('p');
+                const data = objeto.data;
+                const dataFormat = new Date(data);
+                const ano = dataFormat.getFullYear();
+                const mes = ('0' + (dataFormat.getMonth() + 1)).slice(-2);
+                const dia = ('0' + dataFormat.getDate()).slice(-2);
+                const dataString = `${dia}-${mes}-${ano} `;
+                p_data.textContent = dataString;
+                wrapperTags.appendChild(p_data);
+                descricao.appendChild(wrapperTags);
+
+                const title = document.createElement('p');
+                title.classList.add('title');
+                title.textContent = objeto.nome;
+                descricao.appendChild(title);
+
+                const desc = document.createElement('p');
+                desc.classList.add('desc');
+                desc.textContent = objeto.author;
+                descricao.appendChild(desc);
+
+                const button = document.createElement('button');
+                button.classList.add('moreInfo');
+                button.textContent = "Mais informações";
+                card.appendChild(descricao);
+                card.appendChild(button);
+
+                objectDataApi.push({
+                    id: objeto._id,
+                    image: objeto.src,
+                    nome: objeto.nome,
+                    localizacao: objeto.localizacao,
+                    data: dataString,
+                    descricao: objeto.descricao
+                });
+
+                if (objeto.categoria === 'Tecnologia Games e Geeks') {
+                    apiDataElement.appendChild(card);
+                }
+                if (objeto.categoria === 'Congresso e Palestras') {
+                    apiDataElement2.appendChild(card);
+                }
+                if (objeto.categoria === 'Infantil') {
+                    apiDataElement3.appendChild(card);
+                }
+                else {
+                    // console.log('categoria não existente')
+                }
+            });
+
+            const cards = document.querySelectorAll('.card');
+            cards.forEach(card => {
+                card.addEventListener('click', () => {
+                    const cardNome = card.getAttribute('data-nome');
+                    const cardSelecionado = objectDataApi.find(data => data.nome === cardNome);
+
+                    // Corrigir o nome da variável aqui para cardSelecionado
+                    document.getElementById('image').setAttribute('src', cardSelecionado.image)
+                    document.getElementById('modal-location').textContent = cardSelecionado.localizacao;
+                    document.getElementById('modal-date').textContent = cardSelecionado.data;
+                    document.getElementById('modal-description').textContent = cardSelecionado.descricao;
+
+                    modal.style.display = 'block';
+                });
+            });
+
             
-                const title = document.createElement('p')
-                title.classList.add('title')
-                title.textContent = objeto.nome
-                descricao.appendChild(title)
-            
-                const desc = document.createElement('p')
-            
-                desc.classList.add('desc')
-                desc.textContent = objeto.author
-                descricao.appendChild(desc)
-            
-                const button = document.createElement('button')
-                button.classList.add('moreInfo')
-                button.textContent = "Mais informações"
-                card.appendChild(descricao)
-                card.appendChild(button)
-                
-            if(objeto.categoria === 'Tecnologia Games e Geeks'){
-                apiDataElement.appendChild(card)
-                
-            }
-            if(objeto.categoria === 'Congresso e Palestras'){
-                apiDataElement2.appendChild(card)
-            }
-            if(objeto.categoria === 'Infantil'){
-                apiDataElement3.appendChild(card)
-            }
-            else {
-                // console.log('categoria não existente')
-            }
-            const moreInfo = document.querySelectorAll(".moreInfo")
-            // console.log(moreInfo)
+            const moreInfo = document.querySelectorAll(".moreInfo");
             const modal = document.getElementById("modal");
             const modalClose = document.getElementById("modal-close");
-            moreInfo.forEach(btn =>{
-                btn.addEventListener("click", (e)=>{
-                if( modal.style.display == "none"){
-                    modal.style.display = "block"
+            moreInfo.forEach(btn => {
+                btn.addEventListener("click", (e) => {
+                    // e.stopPropagation
+                    if (modal.style.display == "none") {
+                        modal.style.display = "block";
+                    } else {
+                        modal.style.display = "none";
+                    }
+                    modalClose.addEventListener("click", closeModal);
 
-                }else{
-                    modal.style.display = "none"
-                }
-                modalClose.addEventListener("click", closeModal)
-                // Fechar o modal quando clicar fora da caixa modal
-                function closeModal() {
-                    modal.style.display = "none";
-                }
-                window.addEventListener("click", (event) => {
-                if (event.target === modal) {
-                    closeModal();
-                }   
+                    function closeModal() {
+                        modal.style.display = "none";
+                    }
+                    window.addEventListener("click", (event) => {
+                        if (event.target === modal) {
+                            closeModal();
+                        }
+                    });
+                });
             });
-        });
-        const btnFav = document.querySelectorAll('.fa-star')
-
-        btnFav.forEach(star =>{
-            star.addEventListener('click', ()=>{
-                console.log('bunda')
-                const isFav = star.classList.toggle('fa-solid')
-                star.setAttribute('favorite', isFav)
-            })
+            const btnFav = document.querySelectorAll('.fa-star');
+            btnFav.forEach(star => {
+                star.addEventListener('click', (e) => {
+                    e.stopPropagation()
+                    console.log('bunda');
+                    const isFav = star.classList.toggle('fa-solid');
+                    cards.setAttribute('favorite', isFav);
+                });
+            });
         })
-    })
-})
-    })
-    .catch(e=>{
-        console.error('Erro: ', e )
-})
+        .catch(e => {
+            console.error('Erro: ', e);
+        });
+});
 
-// function gerarModal(img, ){
-//     const modal = document.createElement('div');
-//     modal.id = 'modal';
-//     modal.classList.add('modal');
 
-//     const modalContent = document.createElement('div');
-//     modalContent.classList.add('modal-content');
-
-//     const closeSpan = document.createElement('span');
-//     closeSpan.classList.add('close');
-//     closeSpan.id = 'modal-close';
-//     closeSpan.innerHTML = '&times;';
-
-//     const imgContainer = document.createElement('div');
-//     imgContainer.classList.add('img');
-//     const img = document.createElement('img');
-//     img.src = 'img/maxresdefault.jpg';
-//     img.alt = '';
-
-//     const h2 = document.createElement('h2');
-//     h2.textContent = 'Detalhes do Evento';
-
-//     const locationParagraph = document.createElement('p');
-//     locationParagraph.innerHTML = 'Localização: <span id="modal-location"></span>';
-
-//     const dateParagraph = document.createElement('p');
-//     dateParagraph.innerHTML = 'Data: <span id="modal-date"></span>';
-
-//     const descriptionParagraph = document.createElement('p');
-//     descriptionParagraph.innerHTML = 'Descrição: <span id="modal-description"></span>';
-
-//     const btnPagesContainer = document.createElement('div');
-//     btnPagesContainer.classList.add('btn-pages');
-
-//     const officialPageButton = document.createElement('button');
-//     officialPageButton.id = 'page-ofc';
-//     officialPageButton.textContent = 'Página oficial';
-
-//     const openMapsLink = document.createElement('a');
-//     openMapsLink.id = 'open-maps';
-//     openMapsLink.classList.add('map-button');
-//     openMapsLink.textContent = 'Abrir Maps';
-
-//     // Anexa os elementos filhos aos elementos pais
-//     imgContainer.appendChild(img);
-//     btnPagesContainer.appendChild(officialPageButton);
-//     btnPagesContainer.appendChild(openMapsLink);
-
-//     modalContent.appendChild(closeSpan);
-//     modalContent.appendChild(imgContainer);
-//     modalContent.appendChild(h2);
-//     modalContent.appendChild(locationParagraph);
-//     modalContent.appendChild(dateParagraph);
-//     modalContent.appendChild(descriptionParagraph);
-//     modalContent.appendChild(btnPagesContainer);
-
-//     modal.appendChild(modalContent);
-
-// }
-
-// btnFav.addEventListener('click', ()=>{
-//     if(btnFav.contains('fa-regular')){
-//         btnFav.classList.remove('fa-regular')
-//         btnFav.classList.add('fa-solid')
-//     } else {
-//         btnFav.classList.add('fa-regular')
-//         btnFav.classList.remove('fa-solid')
-//     }
-// })
-
-// axios.get(apiUrl)
-// .then(response =>{
-//     const data = response.data
-//     const natalConteiner = document.getElementById('natal-conteiner')
-
-//     const   
-
-// }).catch(e=>{
-//     console.error('Erro: '+e)
-// })
